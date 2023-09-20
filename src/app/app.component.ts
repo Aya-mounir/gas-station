789c2e8723949ec4ce3a136a177c1a298f1114c9
work <aya.mounir167@gmail.com>
2023-09-20 21:44:19 +0300

    dddddddddd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    salonName: string='ppink';
+    siteName: any = 'بينك'; // Initialize with a default name
+    salonImage: any = '../apple-touch-icon.png'
+  constructor(private meta: Meta, private title: Title){}
+  updateTitle(title: string = 'Pink') {
+    if (title) {
+        this.title.setTitle(title);
+    }
+}
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  // var link:HTMLLinkElement = document.querySelector('link[rel="icon"]');
+  this.updateTitle()
+  this.meta.addTag({ name: 'description', content: 'بينك سيستم لخدمات التجميل و السبا' });
+  this.meta.updateTag({ name: 'description', content: 'بينك سيستم لخدمات التجميل و السبا' });
+  // link.href = `./../apple-touch-icon.png`;
+}
 }
